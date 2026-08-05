12b4feeb97e5f31750cc9942b57422c51fb94aad
Taki2006 <yahia.nouar2@gmail.com>
2026-08-05 11:54:26 +0100

    Move jest.config.js to Project-Code/Program/

---
+++ Project-Code/Program/jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+};
